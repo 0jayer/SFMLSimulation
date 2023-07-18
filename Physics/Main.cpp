8959bfdeea2bc369b4a363d9b80e2311129dbdce
0jayer <100233258+0jayer@users.noreply.github.com>
2023-07-18 21:40:21 +0600

    Update Main.cpp

--- Physics/Main.cpp
+++ Physics/Main.cpp
@@ -4,8 +4,8 @@ struct Object {
 	sf::CircleShape circle;
 	sf::Vector2f velocity = sf::Vector2f(0.f,0.f);
 };
-const int gravity = 9.8f;
-const int damp = 1.0f;
+const float gravity = 9.8;
+const float damp = 0.89;
 
 void display(Object& circle,sf::RenderWindow& window)
 {	
@@ -13,7 +13,7 @@ void display(Object& circle,sf::RenderWindow& window)
 	window.setActive(true);
 	circle.circle.setRadius(25);
 	circle.circle.setFillColor(sf::Color::Blue);
-	circle.circle.setPosition(600, 300);
+	circle.circle.setPosition(0, 0);
 }
 
 void moveY(Object& circle,sf::RenderWindow& window, float dt)
@@ -21,11 +21,17 @@ void moveY(Object& circle,sf::RenderWindow& window, float dt)
 	circle.velocity.y += gravity*dt;
 	circle.circle.move(circle.velocity);
 	int diameter = 2 * circle.circle.getRadius();
-		if (circle.circle.getPosition().y + diameter >= window.getSize().y)
+		if (circle.circle.getPosition().y <= 0)
+		{
+			circle.velocity.y = -circle.velocity.y * damp;
+			circle.circle.setPosition(circle.circle.getPosition().x , 0);
+		}
+		else if (circle.circle.getPosition().y + diameter >= window.getSize().y)
 		{
 			circle.velocity.y = -circle.velocity.y * damp;
 			circle.circle.setPosition(circle.circle.getPosition().x, window.getSize().y - diameter);
 		}
+
 }
 
 void moveX(Object& circle, sf::RenderWindow& window, float dt)
@@ -33,7 +39,12 @@ void moveX(Object& circle, sf::RenderWindow& window, float dt)
 	circle.velocity.x += gravity * dt;
 	circle.circle.move(circle.velocity);
 	int diameter = 2 * circle.circle.getRadius();
-	if (circle.circle.getPosition().x + diameter >= window.getSize().x)
+	if (circle.circle.getPosition().x <= 0)
+	{
+		circle.velocity.x = -circle.velocity.x * damp;
+		circle.circle.setPosition(0, circle.circle.getPosition().y);
+	}
+	else if (circle.circle.getPosition().x + diameter >= window.getSize().x)
 	{
 		circle.velocity.x = -circle.velocity.x * damp;
 		circle.circle.setPosition(window.getSize().x - diameter, circle.circle.getPosition().y);
@@ -47,7 +58,9 @@ int main()
 	Object circle2, circle3;
 	display(circle, window);
 	display(circle2, window);
+	circle2.circle.setPosition(600, 0);
 	display(circle3, window);
+	circle3.circle.setPosition(0,300);
 	sf::Clock clock;
 	sf::Time dt;
 	while (window.isOpen())
@@ -63,7 +76,9 @@ int main()
 		moveY(circle,window,dt.asSeconds());
 		circle2.circle.setFillColor(sf::Color::Green);
 		moveX(circle2, window, dt.asSeconds());
+		moveY(circle2, window, dt.asSeconds());
 		circle3.circle.setFillColor(sf::Color::Red);
+		moveX(circle3, window, dt.asSeconds());
 		moveY(circle3, window, dt.asSeconds());
 		window.clear(sf::Color::White);
 		window.draw(circle.circle);
